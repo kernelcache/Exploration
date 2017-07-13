7dd06cc356ecd4e31dc8e75fe7c4c9cb13c8ff9e
kernelcache <dholland2000@hotmail.com>
2017-07-13 06:10:31 -0700

    Add new code

---
+++ SimpleEcho/SimpleEcho/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleEcho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Count() > 0)
+            {
+                Console.WriteLine(args[0]);
+            }
+            else
+            {
+                Console.WriteLine("No argument passed");
+            }
+        }
+    }
+}
